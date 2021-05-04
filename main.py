d90bdf2cdc265a8aae68b64e1982911da561da02
BREgitLEEhub2222 <>
2021-05-04 22:54:31 +0000

    added uptime stuff

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import time 
+with open('uptime.txt', 'a') as file:
+  with open('time.txt', 'r') as filet:
+    file.write((', ' + str(round(time.time()) - float(filet.read()))))
+with open('time.txt', 'w') as file:
+  file.write(str(time.time()))
 from encrypt import decrint, encrint
 with open('conv.py', 'w') as f:
   with open('totaly just numbers.txt', 'r') as g:
